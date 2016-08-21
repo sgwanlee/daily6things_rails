e07640f67c50e371f9e30f61b351dbdb8d242b57
Seonggwan Lee <seonggwan.lee@gmail.com>
2016-08-21 21:20:49 +0900

    change bg-color when mouse is over delete link

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -3,6 +3,12 @@ jQuery.fn.submitOnCheck = function() {
   this.find('input[type=checkbox]').click(function() {
     $(this).parent('form').submit();
   });
+  this.find('.delete-link').mouseover(function(){
+    $(this).parent('form').css('background-color', '#eee');
+  });
+  this.find('.delete-link').mouseout(function(){
+    $(this).parent('form').css('background-color', '');
+  });
   return this;
 }
 
