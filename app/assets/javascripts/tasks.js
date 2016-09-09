d745b18afdd82e0704e80e3b4fe72721225053b7
SG LEE <sg.lee@perples.com>
2016-09-09 14:29:56 +0900

    - Change UI - replace javascript tag

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,13 +1,6 @@
 jQuery.fn.submitOnCheck = function() {
-  this.find('input[type=submit]').remove();
-  this.find('input[type=checkbox]').click(function() {
+  this.find('input[type=checkbox]').change(function() {
     $(this).parent('form').submit();
-  });
-  this.find('.delete-link').mouseover(function(){
-    $(this).parent('form').css('background-color', '#eee');
-  });
-  this.find('.delete-link').mouseout(function(){
-    $(this).parent('form').css('background-color', '');
   });
   return this;
 }
